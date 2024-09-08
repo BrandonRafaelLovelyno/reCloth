1e8abee954066f8b92a2adfecb327af9185cf125
deren21 <109866900+deren21@users.noreply.github.com>
2024-09-08 22:13:27 +0700

    feat/add order, payment, & taylorcontract class

---
+++ reCloth/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reCloth
+{
+    class Order
+    {
+        private Customer customer;
+        private int price;
+        private string image;
+        private string specification;
+        private Boolean isDone;
+        private string title;
+    }
+}
---
+++ reCloth/TaylorContract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reCloth
+{
+    class TaylorContract
+    {
+        private Order order;
+        private Taylor taylor;
+        private Proposal proposal;
+        private Payment payment;
+    }
+}
