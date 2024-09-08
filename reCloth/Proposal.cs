e9aba59b0f4f9c8cc20cb5d11ae8a84e07fd13fa
difz <muhammadhifzhonharundoyo@mail.ugm.ac.id>
2024-09-09 05:55:54 +0700

    feat/add proposal,taylor, & customer class

---
+++ reCloth/Proposal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reCloth
+{
+    internal class Proposal
+    {
+        private enum role;
+        private bool isAccepted;
+        private int price;
+
+    }
+}
