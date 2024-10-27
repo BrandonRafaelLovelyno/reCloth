3b0dcebae16ecda769bbf3b8875c09d5f02eb75b
deren21 <109866900+deren21@users.noreply.github.com>
2024-10-27 14:13:12 +0700

    feat: integrate postgre to app

---
+++ Interface/DatabaseHelper.cs
@@ -0,0 +1,49 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace Interface
+{
+    internal class DatabaseHelper
+    {
+        private string connectionString;
+        private NpgsqlConnection conn;
+
+        public DatabaseHelper()
+        {
+            // Initialize ConfigurationBuilder to read the connection string from appsettings.json
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Access the connection string in the "AppSettings" section with key "aivenDB"
+            this.connectionString = config.GetSection("AppSettings")["aivenDB"];
+            conn = new NpgsqlConnection(connectionString);
+        }
+
+        public void TestConnection()
+        {
+
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Connection successful");
+
+                conn.Close();
+                Console.WriteLine("Connection closed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+         
+        }
+    }
+}
