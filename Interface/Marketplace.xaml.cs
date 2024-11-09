77a116bcbb13f2986b8e0cd46ec91628e7045e9c
deren21 <109866900+deren21@users.noreply.github.com>
2024-11-09 17:22:36 +0700

    feat: integrate marketplace page with order page

--- Interface/Marketplace.xaml.cs
+++ Interface/Marketplace.xaml.cs
@@ -26,5 +26,16 @@ namespace Interface
         {
             InitializeComponent();
         }
+
+        private void Route_to_OrderPage(object sender, MouseButtonEventArgs e)
+        {
+            var appWindow = Application.Current.MainWindow as AppWindow;
+            Console.WriteLine("Try to navigate to OrderPage");
+            if (appWindow != null)
+            {
+                appWindow.MainFrame.NavigationService.Navigate(new OrderPage(11));
+                Console.WriteLine("Navigating to OrderPage");
+            }
+        }
     }
 }
